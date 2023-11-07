68ff0f72db5d35bb4f1ff1f2fb7fdebea52ff1e6
Hiroshi Yamashita <yama01hiro@gmail.com>
2023-11-07 09:43:20 +0900

    :wrench: 従業員、出勤・退勤および休憩用のモデルとコントローラ、マイグレーションファイルの追加

---
+++ src/database/migrations/2023_11_06_080324_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->string('email', 255)->unique();
+            $table->tinyInteger('role')->unsigned();
+            $table->string('password', 255);
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}

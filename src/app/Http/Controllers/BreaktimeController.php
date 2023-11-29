75c5923dce16a99171bd23fe5121fa7e11d91f30
Hiroshi Yamashita <yama01hiro@gmail.com>
2023-11-29 13:45:20 +0900

    :recycle: コントローラーとモデルの内容を見直した

--- src/app/Http/Controllers/BreaktimeController.php
+++ src/app/Http/Controllers/BreaktimeController.php
@@ -3,75 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Breaktime;
-use Illuminate\Http\Request;
 
 class BreaktimeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Breaktime  $breaktime
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Breaktime $breaktime)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Breaktime  $breaktime
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Breaktime $breaktime)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Breaktime  $breaktime
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Breaktime $breaktime)
-    {
-        //
-    }
-
     /**
      * 指定された休憩レコードを削除する。
      *
--- src/app/Models/Breaktime.php
+++ src/app/Models/Breaktime.php
@@ -9,8 +9,31 @@ class Breaktime extends Model
 {
     use HasFactory;
 
+    // --------------------------------------------------------------------------------
+    // モデル属性とリレーションシップ
+    // --------------------------------------------------------------------------------
+
+    /**
+     * マスアサインメントで使用可能な属性。
+     *
+     * この属性リストを通じて、createやupdateメソッドなどで一括して割り当て可能なモデルの属性を定義する。
+     * - `attendance_id` : 出勤・退勤ID
+     * - `start_time` : 休憩開始時刻
+     * - `end_time` : 休憩終了時刻
+     *
+     * @var array
+     */
     protected $fillable = ['attendance_id', 'start_time', 'end_time'];
 
+    /**
+     * この休憩レコードに関連付けられている勤怠エンティティを取得するリレーションシップ。
+     *
+     * `Attendance` モデルとの "BelongsTo" 関連を定義する。
+     * これにより、特定の休憩レコードに関連付けられている勤怠情報にアクセスできるようになる。
+     * （例えば、休憩レコードから直接関連する勤怠の日付や他の属性にアクセスする場合に使用）
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo 休憩レコードに関連する勤怠モデルへのリレーションシップ
+     */
     public function attendance()
     {
         return $this->belongsTo(Attendance::class);
